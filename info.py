1c9f4ae41d9d440d0768346217ddecb198599a02
chetanmehar <119054386+chetanmehar@users.noreply.github.com>
2024-10-15 09:13:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://political-florinda-mehar1-c3d7a6f7.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
